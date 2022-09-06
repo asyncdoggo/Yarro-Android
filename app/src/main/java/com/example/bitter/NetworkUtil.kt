f1900c9bd1d47f4289d96433e05799c3ea5c679a
asyncdoggo <ayushdeshpande81@gmail.com>
2022-09-06 22:16:41 +0530

    Add postImage

--- app/src/main/java/com/example/bitter/NetworkUtil.kt
+++ app/src/main/java/com/example/bitter/NetworkUtil.kt
@@ -1,12 +1,25 @@
 package com.example.bitter
 
-import kotlinx.coroutines.CoroutineScope
-import okhttp3.Callback
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import androidx.core.net.toFile
+import coil.annotation.ExperimentalCoilApi
+import coil.imageLoader
+import coil.memory.MemoryCache
+import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.RequestBody.Companion.asRequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
+import okio.IOException
 import org.json.JSONObject
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 
 fun postForm(
@@ -23,4 +36,69 @@ fun postForm(
 
     val okHttpClient = OkHttpClient()
     okHttpClient.newCall(request).enqueue(callback)
+}
+
+fun postImage(
+    context :Context,
+    bitmap: Bitmap,
+    uname:String,
+    key:String
+) {
+    val fileData = MultipartBody.Builder()
+        .setType(MultipartBody.FORM)
+        .addFormDataPart("uname",uname)
+        .addFormDataPart("key",key)
+        .addFormDataPart(
+            "image",
+            filename = "$uname.png",
+            bitmapToPng(context,bitmap).asRequestBody("image/*".toMediaTypeOrNull())
+        )
+        .build()
+
+        val request = Request.Builder()
+            .url("$postUrl/sendimage")
+            .post(fileData)
+            .build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseString = String(response.body.bytes())
+                val ret = JSONObject(responseString)
+
+                println(ret)
+            }
+
+        })
+}
+
+fun bitmapToPng(context: Context, bitmap:Bitmap): File {
+    //create a file to write bitmap data
+    val f = File(context.cacheDir, "tempfile.png");
+    f.createNewFile();
+
+//Convert bitmap to byte array
+    val bos = ByteArrayOutputStream();
+    bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+    val bitmapdata = bos.toByteArray();
+
+//write the bytes in file
+    val fos = FileOutputStream(f);
+    fos.write(bitmapdata);
+    fos.flush();
+    fos.close();
+
+    return f
+}
+
+@OptIn(ExperimentalCoilApi::class)
+fun removeCoilCache(context: Context,url:String){
+    val imageLoader = context.imageLoader
+    imageLoader.diskCache?.remove(url)
+    imageLoader.memoryCache?.remove(MemoryCache.Key(url))
 }
