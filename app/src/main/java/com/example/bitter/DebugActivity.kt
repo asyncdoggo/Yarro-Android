6b7ee4ce7a0a4bfd0926dd7dbf879258567340b1
asyncdoggo <ayushdeshpande81@gmail.com>
2022-09-06 22:13:54 +0530

    Add DebugActivity.kt

---
+++ app/src/main/java/com/example/bitter/DebugActivity.kt
@@ -0,0 +1,65 @@
+package com.example.bitter
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+
+class DebugActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            setContent{
+                DebugPage()
+            }
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun DebugPage() {
+
+    var url by remember {
+        mutableStateOf("")
+    }
+    val context = LocalContext.current
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+           modifier = Modifier.fillMaxWidth()
+        ) {
+            TextField(
+                value = url,
+                onValueChange = {url = it}
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Button(onClick = {
+                val intent = Intent(context, MainActivity::class.java)
+                intent.putExtra("url",url)
+                context.startActivity(intent)
+            }) {
+                Text("next")
+            }
+        }
+    }
+}
