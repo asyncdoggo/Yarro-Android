d6a354700ad555dbad2299a48b591131046b8d91
asyncdoggo <ayushdeshpande81@gmail.com>
2022-10-07 09:33:58 +0530

    Add routes for bottom navigation

--- app/src/main/java/com/example/bitter/data/Routes.kt
+++ app/src/main/java/com/example/bitter/data/Routes.kt
@@ -1,12 +1,28 @@
 package com.example.bitter.data
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Chat
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.SupervisedUserCircle
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(
+    var title: String,
+    var icon: ImageVector,
+    var route:String
+)
+
 sealed class Routes(
     val route:String
 ){
     object LoginScreen : Routes("loginScreen")
     object RegisterScreen : Routes("registerScreen")
-    object UserDetailsScreen : Routes("userDetailsScreen")
     object ForgotPassScreen : Routes("forgotPassScreen")
     object MainScreen : Routes("MainScreen")
-    object UserProfileScreen : Routes("UserProfileScreen")
+    object EditUserProfileScreen : Routes("EditUserProfileScreen")
+    object NewPostScreen : Routes("NewPostScreen")
+
+    object Home: BottomNavItem("home", Icons.Default.Home,"home")
+    object Profile: BottomNavItem("profile", Icons.Default.SupervisedUserCircle,"profile")
+    object Chat: BottomNavItem("chat", Icons.Default.Chat,"chat")
 }
